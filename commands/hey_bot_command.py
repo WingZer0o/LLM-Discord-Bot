5ec707ef22bd077cf0dbbcdfda26fca9b2b4ec8f
WingZer0o <mikemulchrone987@gmail.com>
2025-01-25 20:30:03 -0500

    chat template

--- commands/hey_bot_command.py
+++ commands/hey_bot_command.py
@@ -3,7 +3,11 @@ from langchain_ollama.llms import OllamaLLM
 
 class HeyBotCommand:
     async def handle_hey_bot_command(ctx):
-        template = """Question: {question}"""
+        print(ctx.message.author)
+        template = """You are a helpfulp chat assistant that is knowledgable about mainly programming. This is my Discord chat channel, where I host documentation for the Cryptographic API Services Github Organization.
+        It is a cryptography project that encompasses C#, TypeScript, and Rust. In some instances, a chat history will be 
+        provided to you from the user who is chatting with you, you are to answer questions to the best of your ability. There is such thing as a newbie question, you are to answer them all.
+        Question: {question}"""
         prompt = ChatPromptTemplate.from_template(template)
         model = OllamaLLM(model="llama3.2")
         chain = prompt | model
